0fb0a9674276c34ab8f347a14b4407d45a3ccadb
Nilesh choudhary <121803888+81nilesh@users.noreply.github.com>
2025-01-03 13:23:57 -0800

    register captain done

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,6 +1,6 @@
 const userModel = require("../models/user.models");
 const userService = require("../services/user.service");
-const {vaildationResult, validationResult} = require("express-validator");
+const {validationResult} = require("express-validator");
 const blackListTokenModel = require("../models/blacklistToken.model");
 
 
@@ -9,8 +9,14 @@ const errors = validationResult(req);
 if(!errors.isEmpty()){
     return res.status(400).json({ errors: errors.array()});
 }
-console.log(req.body);
 const {fullname, email, password} = req.body;
+
+const isUserAlready = await userModel.findOne({ email });
+
+if(isUserAlready){      
+    return res.status(400).json({message: "User already exist"});
+}
+
 const hashedPassword = await userModel.hashPassword(password);
 
 
